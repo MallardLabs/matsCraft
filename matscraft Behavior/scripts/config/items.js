343bd68a7357151813030132806c5e84f25d5668
zaapr0x <132854424+zaapr0x@users.noreply.github.com>
2025-04-23 02:24:29 +0700

    Add files via upload

---
+++ matscraft Behavior/scripts/config/items.js
@@ -0,0 +1,26 @@
+export const PICKAXES = [
+  {
+    id: "matscraft:nando_pickaxe",
+    name: "Nando Pickaxe",
+    cost: 50,
+    icon: "textures/items/pickaxe/nando.png",
+    message:
+      "§lItem Name§r: Nando Pickaxe\n§lCost: §r50 Mats\n\n§lAbility\n§rBy Using Nando Pickaxe, you can get mats drops from common,uncommon blocks",
+  },
+  {
+    id: "matscraft:lowpolyduck_pickaxe",
+    name: "lowpolyduck Pickaxe",
+    cost: 150,
+    icon: "textures/items/pickaxe/lowpolyduck.png",
+    message:
+      "§lItem Name§r: lowpolyduck Pickaxe\n§lCost: §r100 Mats\n\n§lAbility:\n§rBy Using lowpolyduck Pickaxe, you can get mats drops from common,uncommon,rare,epic blocks",
+  },
+  {
+    id: "matscraft:mezo_pickaxe",
+    name: "Mezo Pickaxe",
+    cost: 200,
+    icon: "textures/items/pickaxe/mezo.png",
+    message:
+      "§lItem Name§r: Mezo Pickaxe\n§lCost: §r200 Mats\n\n§lAbility:\n§rBy Using Mezo Pickaxe, you can get mats drops from common,uncommon,rare,epic,legendary blocks",
+  },
+];
