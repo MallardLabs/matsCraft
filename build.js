876a02fad5be2ada7aee3170f5b46eb1e067b9aa
RZ <neobyteid@gmail.com>
2025-04-26 21:56:04 +0700

    .

--- build.js
+++ build.js
@@ -1,13 +1,10 @@
 import zl from "zip-lib";
-console.log("Creating BP mcpack...");
-zl.archiveFolder("dist/matscraft Behavior", "build/matscraft(BP).mcpack").then(function () {
-  console.log("done");
+
+const zip = new zl.Zip();
+
+zip.addFolder("dist");
+zip.archive("build/matscraft.mcaddon").then(function () {
+    console.log("done");
 }, function (err) {
-  console.log(err);
-});
-console.log("Creating RP mcpack...");
-zl.archiveFolder("dist/matscraft Resources", "build/matscraft(RP).mcpack").then(function () {
-  console.log("done");
-}, function (err) {
-  console.log(err);
+    console.log(err);
 });
