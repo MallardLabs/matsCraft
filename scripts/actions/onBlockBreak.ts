474f5847ec74a9ae41f4a398425884add4fcfb28
RZ <neobyteid@gmail.com>
2025-05-25 21:50:56 +0700

    fix misspelled nanndo's name & fix secret headers

--- scripts/actions/onBlockBreak.ts
+++ scripts/actions/onBlockBreak.ts
@@ -7,10 +7,11 @@ import { world } from "@minecraft/server";
 
 import httpReq from "../lib/httpReq";
 import { getPlayerData } from "../utils/playerUtils";
+import genSecret from "../lib/genSecret";
 
 const pickAxeAbility = [
   {
-    typeId: "matscraft:nando_pickaxe",
+    typeId: "matscraft:nanndo_pickaxe",
     allowed: ["matscraft:common_mats_ore", "matscraft:uncommon_mats_ore"],
   },
   {
@@ -101,7 +102,10 @@ const updateBlock = async (data: any) => {
       method: "POST",
       url: CONFIG.INSERT_BLOCK,
       body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        matscraft_token: genSecret(),
+      },
     });
 
     if (response.status === 200) {
