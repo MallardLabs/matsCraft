c678b1a6d549cb3171738235f877c2b893a08e08
RZ <neobyteid@gmail.com>
2025-06-18 16:51:07 +0700

    add launcher for admin menu

--- behavior_packs/matscraft(BP)/scripts/actions/onItemUse.js
+++ behavior_packs/matscraft(BP)/scripts/actions/onItemUse.js
@@ -1,8 +1,13 @@
 import { world } from "@minecraft/server";
 import showMainMenu from "../gui/main";
+import admin from "../gui/admin";
 world.afterEvents.itemUse.subscribe(async (data) => {
     const player = data.source;
     if (data.itemStack.typeId === "matsphone:matsphone") {
-        showMainMenu(player);
+        if (!player.hasTag("admin")) {
+            showMainMenu(player);
+            return;
+        }
+        admin(player);
     }
 });
